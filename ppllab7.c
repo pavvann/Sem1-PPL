6094c878e7f32c75d6cd5c6421a9c7ac5b1b5a74
Pawan <pawanbajaj2012@gmail.com>
2021-12-22 07:03:41 +0530

    exp 7 solutions

--- ppllab7.c
+++ ppllab7.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/*
-//3
+
+/*3
 //Convert uppercase string to lowercase using for loop
 
 int main()
@@ -20,15 +20,10 @@ int main()
 
     printf("%s", a);
     return 0;   
-}
-*/
+}*/
 
 
-
-
-
-
-//1
+/*1
 //Find sum of all array elements using recursion.
 
 int sum(int a[], int n)
@@ -39,8 +34,7 @@ int sum(int a[], int n)
     } 
     else 
     {
-
-    return a[n-1]+sum(a,n-1);
+        return a[n-1]+sum(a,n-1);
     }
 }
 
@@ -58,4 +52,68 @@ int  main()
     printf("%d", sum(a, n));
     return 0;
         
+}*/
+
+
+//2
+
+int main()
+{
+    int a1[100];
+    int n;
+    printf("enter the number of elements in your array (max 100): ");
+    scanf("%d", &n);
+
+    printf("\nenter the elements of the array: \n");
+    for (int k = 0; k < n; k++)
+    {
+        scanf("%d", &a1[k]);
+    }
+
+    printf("\nthe array you entered is: ");
+    for (int k = 0; k < n; k++)
+    {
+        printf("%d ", a1[k]);
+    }
+    
+    int x, i;
+    printf("\n\nenter the number you want to insert: ");
+    scanf("%d", &x);
+    printf("enter the position of your new number in the array: ");
+    scanf("%d", &i);
+
+    n = n+1;
+
+    for (int k = n - 1; k > i - 1; k--)
+    {
+        a1[k] = a1[k-1];
+    }
+    a1[i-1] = x;
+
+    printf("\n\nupdated array: ");
+    for (int k = 0; k < n; k++)
+    {
+        printf("%d ", a1[k]);
+    }
+    
+    int j;
+    printf("\n\nenter the position of element you want to delete: ");
+    scanf("%d", &j);
+    
+
+    for (int k = j-1; k < n; k++)
+    {
+        a1[k] = a1[k+1];
+    }
+    n--;
+
+    printf("\n\nupdated array: ");
+    for (int k = 0; k < n; k++)
+    {
+        printf("%d ", a1[k]);
+    }
+    
+
+    
+    
 }
